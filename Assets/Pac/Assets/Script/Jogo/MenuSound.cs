816968ffe3b49caf031213ce8438e1ae40e09647
Pedro Oliveira <pedrorodriigues@live.com>
2020-08-24 15:46:53 -0300

    2.3
    mute/unmute
    melhoria nos menus
    organização

--- Assets/Pac/Assets/Script/Jogo/MenuSound.cs
+++ Assets/Pac/Assets/Script/Jogo/MenuSound.cs
@@ -1,15 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuSound : MonoBehaviour
 {
 
     public AudioSource menuSelectSound;
+    public AudioSource menuMainSong;
+    public GameObject mute, unmute;
 
     public void playSelectMenu()
     {
+        
         menuSelectSound.Play();
+    }
+
+    public void PauseResume()
+    {
+        if (menuMainSong.isPlaying)
+        {
+            menuMainSong.Pause();
+            mute.SetActive(true);
+            unmute.SetActive(false);
+        }
+
+        else{
+            menuMainSong.Play();
+            mute.SetActive(false);
+            unmute.SetActive(true);
+        }
+           
     }
 }
 
